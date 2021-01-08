d9d861657b5d2f9ee2eef3ecc46242e5abe73628
bnula <bnulagnula@gmail.com>
2021-01-08 13:30:14 +0100

    implemented Interface and created constructor in CharacterRepository.cs

--- player-log/Repository/CharacterRepository.cs
+++ player-log/Repository/CharacterRepository.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using player_log.Contracts;
+using player_log.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace player_log.Repository
 {
-    public class CharacterRepository
+    public class CharacterRepository : ICharacterRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CharacterRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(Character entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Character entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Character> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Character entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
