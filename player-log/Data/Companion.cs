0709d9e892a31c85539631d10bcc198a1c3c2240
bnula <bnulagnula@gmail.com>
2021-01-08 12:04:21 +0100

    added properties to Companion class

--- player-log/Data/Companion.cs
+++ player-log/Data/Companion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace player_log.Data
 {
     public class Companion
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cr { get; set; }
+        public string CompanionClass { get; set; }
+        public string Description { get; set; }
+
     }
 }
