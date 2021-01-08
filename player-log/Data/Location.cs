36e07b5e5886817046d5d179fbe24099088199c4
bnula <bnulagnula@gmail.com>
2021-01-08 12:24:44 +0100

    added properties to Location class

--- player-log/Data/Location.cs
+++ player-log/Data/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace player_log.Data
 {
     public class Location
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Population { get; set; }
+        public string Description { get; set; }
     }
 }
