e2d8a3973ae36242c7131bc8644fb74cbc8f5854
bnula <bnulagnula@gmail.com>
2021-01-09 11:56:35 +0100

    defined basic functions in CharacterRepository.cs

--- player-log/Repository/CharacterRepository.cs
+++ player-log/Repository/CharacterRepository.cs
@@ -18,32 +18,44 @@ namespace player_log.Repository
 
         public bool Create(Character entity)
         {
-            throw new NotImplementedException();
+            // create the record in the Db, save changes and return true if there was a change
+            _db.Characters.Add(entity);
+            return Save();
         }
 
         public bool Delete(Character entity)
         {
-            throw new NotImplementedException();
+            // remove the record from the Db, save changes and return true if there was a change
+            _db.Remove(entity);
+            return Save();
         }
 
         public ICollection<Character> FindAll()
         {
-            throw new NotImplementedException();
+            // query the DB table to return a list of all the items in the table
+            var items = _db.Characters.ToList();
+            return items;
         }
 
         public Character FindById(int id)
         {
-            throw new NotImplementedException();
+            // query the DB table to return an item based on the Id
+            var item = _db.Characters.Find(id); // var item = _db.Characters.FirstOrDefault(t => t.Id == id);
+            return item;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            // save any changes done to the Db and return true if there has been at least 1 change
+            var changes = _db.SaveChanges();
+            return changes > 0;
         }
 
         public bool Update(Character entity)
         {
-            throw new NotImplementedException();
+            // update the record in the Db, save changes and return true if there was a change
+            _db.Update(entity);
+            return Save();
         }
     }
 }
