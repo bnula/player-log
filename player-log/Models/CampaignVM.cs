8f91117e7d43ca2020aeac39925f9407c7e8feb5
bnula <bnulagnula@gmail.com>
2021-01-10 11:51:53 +0100

    changed property name from Desc to Decription for Campaign

--- player-log/Models/CampaignVM.cs
+++ player-log/Models/CampaignVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace player_log.Models
 {
     public class CampaignVM
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
