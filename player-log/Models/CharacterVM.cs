ccc6ceec9fbcb49b36107e3f18e5358d673e6cf2
bnula <bnulagnula@gmail.com>
2021-01-10 13:52:04 +0100

    created DetailsCharacterVM class

--- player-log/Models/CharacterVM.cs
+++ player-log/Models/CharacterVM.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace player_log.Models
 {
-    public class CharacterVM
+    public class DetailsCharacterVM
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string MainClass { get; set; }
+        public bool Multiclass { get; set; }
+        public string SecondClass { get; set; }
+        public string ThirdClass { get; set; }
+        public int Level { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Wisdom { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+        public string Campaign { get; set; }
     }
 }
