2ed5ce9cb6a9bc84b6f137598e05ae71c3f3fb24
bnula <bnulagnula@gmail.com>
2021-01-10 11:48:13 +0100

    removed additional restrictions from Character DataModel, will add them in CharacterVM

--- player-log/Data/Character.cs
+++ player-log/Data/Character.cs
@@ -12,50 +12,18 @@ namespace player_log.Data
     {
         [Key]
         public int Id { get; set; }
-        
-        [Required]
-        [DefaultValue("Name")]
         public string Name { get; set; }
-
-        [Required]
-        [DefaultValue("Human")]
         public string Race { get; set; }
-
-        [Required]
-        [DefaultValue("Pick a Class")]
         public string MainClass { get; set; }
-
-        [DefaultValue(false)]
         public bool Multiclass { get; set; }
         public string SecondClass { get; set; }
         public string ThirdClass { get; set; }
-
-        [Required]
-        [DefaultValue(1)]
         public int Level { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Strength { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Dexterity { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Constitution { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Wisdom { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Intelligence { get; set; }
-
-        [Required]
-        [DefaultValue(10)]
         public int Charisma { get; set; }
 
         [ForeignKey("CampaignId")]
