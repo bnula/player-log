a8b37d62cf3e15f6d6bc1b58ced56e3f3464ad13
bnula <bnulagnula@gmail.com>
2021-01-10 12:17:10 +0100

    created Mapper.cs and added mappings for ListCampaignsVM and CreateCampaignVM

---
+++ player-log/Mappings/Mapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using player_log.Data;
+using player_log.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace player_log.Mappings
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            // map the data class to the view class and vice versa
+            CreateMap<Campaign, ListCampaignsVM>().ReverseMap();
+            CreateMap<Campaign, CreateCampaignVM>().ReverseMap();
+        }
+    }
+}
