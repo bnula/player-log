06e5cb9de67d92523bb5adbb83eacdf13bfdb114
bnula <bnulagnula@gmail.com>
2021-01-12 23:23:25 +0100

    added migrations and update Db for CompanionVMs

--- player-log/Data/Migrations/20210112222016_AddedCompanionVMTables.Designer.cs
+++ player-log/Data/Migrations/20210112222016_AddedCompanionVMTables.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using player_log.Data;
 
 namespace player_log.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210112222016_AddedCompanionVMTables")]
+    partial class AddedCompanionVMTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
