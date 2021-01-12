97450e4d22f4188da9eb21292f9005a0fd1b541b
bnula <bnulagnula@gmail.com>
2021-01-12 22:23:04 +0100

    added basic functions into CompanionRepository.cs

--- player-log/Repository/CompanionRepository.cs
+++ player-log/Repository/CompanionRepository.cs
@@ -16,37 +16,55 @@ namespace player_log.Repository
         }
         public bool Create(Companion entity)
         {
-            throw new NotImplementedException();
+            // add the item to the db
+            _db.Add(entity);
+            // save changes to the db
+            return Save();
         }
 
         public bool Delete(Companion entity)
         {
-            throw new NotImplementedException();
+            // remove the item from the db
+            _db.Remove(entity);
+            // save cahnges to the db
+            return Save();
         }
 
         public ICollection<Companion> FindAll()
         {
-            throw new NotImplementedException();
+            // retrieve all of the items from the db table
+            var items = _db.Companions.ToList();
+            return items;
         }
 
         public Companion FindById(int id)
         {
-            throw new NotImplementedException();
+            // retrieve an item from the db table based on the id
+            var item = _db.Companions.Find(id);
+            return item;
         }
 
         public bool RecordExists(int id)
         {
-            throw new NotImplementedException();
+            // check if there is an item in the db based on the id
+            var exists = _db.Companions.Any(q => q.Id == id);
+            return exists;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            // save changes to the db and return number of records changed
+            var changes = _db.SaveChanges();
+            // return if there were any changes
+            return changes > 0;
         }
 
         public bool Update(Companion entity)
         {
-            throw new NotImplementedException();
+            // update the item in the db
+            _db.Update(entity);
+            // save changes to the db
+            return Save();
         }
     }
 }
