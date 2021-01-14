ed245759f30053bc7faeaba2cc612f7fcae408d6
bnula <bnulagnula@gmail.com>
2021-01-14 09:12:09 +0100

    created QuestRepository.cs

---
+++ player-log/Repository/QuestRepository.cs
@@ -0,0 +1,59 @@
+﻿using player_log.Contracts;
+using player_log.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace player_log.Repository
+{
+    public class QuestRepository : IQuestRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public QuestRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(Quest entity)
+        {
+            _db.Quests.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(Quest entity)
+        {
+            _db.Quests.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<Quest> FindAll()
+        {
+            var items = _db.Quests.ToList();
+            return items;
+        }
+
+        public Quest FindById(int id)
+        {
+            var item = _db.Quests.Find(id);
+            return item;
+        }
+
+        public bool RecordExists(int id)
+        {
+            var exists = _db.Quests.Any(q => q.Id == id);
+            return exists;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(Quest entity)
+        {
+            _db.Quests.Update(entity);
+            return Save();
+        }
+    }
+}
