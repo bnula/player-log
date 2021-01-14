17876e8b8919fd09625ce8f95f31185a6c0a9ef6
bnula <bnulagnula@gmail.com>
2021-01-14 09:17:50 +0100

    added Reward property to Quest class and created QuestVM.cs

---
+++ player-log/Data/Migrations/20210114081651_AddedRewardsPropertyToQues.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace player_log.Data.Migrations
+{
+    public partial class AddedRewardsPropertyToQues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reward",
+                table: "Quests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reward",
+                table: "Quests");
+        }
+    }
+}
--- player-log/Models/QuestVM.cs
+++ player-log/Models/QuestVM.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace player_log.Data
+namespace player_log.Models
 {
-    public class Quest
+    public class QuestListVM
     {
-        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Campaign { get; set; }
+    }
+
+    public class QuestDetailsVM
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public string MainObjective { get; set; }
         public string Description { get; set; }
         public string Notes { get; set; }
         public string Reward { get; set; }
-
-        [ForeignKey("CamapignId")]
         public string Campaign { get; set; }
-        public int? CampaignId { get; set; }
     }
 }
